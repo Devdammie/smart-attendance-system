4f81c55469b67fd2b115ede5000b5f026a028d92
Dammie <damilolajimoh2020@gmail.com>
2025-09-20 20:15:01 -0700

    fixed sever error

--- utils/faceapi.js
+++ utils/faceapi.js
@@ -1,7 +1,6 @@
 import * as faceapi from 'face-api.js';
 import * as canvas from 'canvas';
 import path from 'path';
-import fs from 'fs';
 
 // Polyfill for face-api.js to work in Node.js
 const { Canvas, Image, ImageData } = canvas;
@@ -12,35 +11,6 @@ const modelsPath = path.join(process.cwd(), 'models', 'face-api');
 
 // Load models
 export const loadModels = async () => {
-    // --- START DEBUGGING ---
-    // This block will help verify if the model files exist on the server.
-    // You can remove this after the deployment is successful.
-    try {
-        const projectRoot = process.cwd();
-        console.log(`[DEBUG] Project root (process.cwd()): ${projectRoot}`);
-        const rootContents = fs.readdirSync(projectRoot);
-        console.log(`[DEBUG] Contents of project root:`, rootContents);
-
-        const modelsDir = path.join(projectRoot, 'models');
-        console.log(`[DEBUG] Checking for models directory at: ${modelsDir}`);
-        const modelsDirExists = fs.existsSync(modelsDir);
-        console.log(`[DEBUG] Does 'models' directory exist? ${modelsDirExists}`);
-
-        if (modelsDirExists) {
-            const modelsDirContents = fs.readdirSync(modelsDir);
-            console.log(`[DEBUG] Contents of 'models' directory:`, modelsDirContents);
-
-            const faceApiDir = path.join(modelsDir, 'face-api');
-            if (fs.existsSync(faceApiDir)) {
-                const faceApiDirContents = fs.readdirSync(faceApiDir);
-                console.log(`[DEBUG] Contents of 'face-api' directory:`, faceApiDirContents);
-            }
-        }
-    } catch (e) {
-        console.error('[DEBUG] Error during directory listing:', e);
-    }
-    // --- END DEBUGGING ---
-
     try {
         await faceapi.nets.ssdMobilenetv1.loadFromDisk(modelsPath);
         await faceapi.nets.faceLandmark68Net.loadFromDisk(modelsPath);
